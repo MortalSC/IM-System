51cef9cf6d3b61dc33cd5e3f96f5107fba91b365
mortal_scode@163.com <yangfeng7@wps.cn>
2025-01-19 21:28:41 +0800

    fix: 引入使用vendor

---
+++ auth-service/vendor/github.com/MortalSC/IM-System/lib/cache/redis/redis.go
@@ -0,0 +1,68 @@
+package redis
+
+import (
+	"context"
+	"github.com/MortalSC/IM-System/lib/cache"
+	"time"
+)
+import "github.com/go-redis/redis/v8"
+
+type IMRedisCache struct {
+	rdb *redis.Client
+}
+
+// NewRedisCache 创建一个新的 Redis 缓存实例
+// 参数：
+// - addr: Redis 地址
+// - password: Redis 密码
+// - db: Redis 数据库索引
+// 返回值：
+// - cache.Cache: 实现了通用 Cache 接口的 Redis 缓存实例
+// - error: 如果初始化失败，返回错误
+func NewRedisCache(r *redis.Options) (cache.Cache, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     r.Addr,
+		Password: r.Password,
+		DB:       r.DB,
+	})
+
+	// 检查连接是否正常
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return &IMRedisCache{rdb: rdb}, nil
+}
+
+// Put 方法用于将 key-value 数据存入 Redis，并设置过期时间
+// 参数：
+// - ctx: 上下文，用于控制请求的生命周期
+// - key: Redis 中的键
+// - val: Redis 中的值
+// - expire: 数据的过期时间
+// 返回值：
+// - error: 如果操作失败，返回错误信息；成功则返回 nil
+func (rc *IMRedisCache) Put(ctx context.Context, key, val string, expire time.Duration) error {
+	err := rc.rdb.Set(ctx, key, val, expire)
+	if err != nil {
+		return err.Err()
+	}
+	return nil
+}
+
+// Get 方法用于从 Redis 中获取指定 key 的值
+// 参数：
+// - ctx: 上下文，用于控制请求的生命周期
+// - key: 要获取的键
+// 返回值：
+// - string: 获取到的值
+// - error: 如果操作失败，返回错误信息；成功则返回 nil
+func (rc *IMRedisCache) Get(ctx context.Context, key string) (string, error) {
+	res, err := rc.rdb.Get(ctx, key).Result()
+	if err == redis.Nil {
+		// 返回空字符串表示 key 不存在
+		return "", nil
+	}
+	return res, err
+}
---
+++ auth-service/vendor/github.com/MortalSC/IM-System/lib/router/run.go
@@ -0,0 +1,54 @@
+package router
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func RunServer(r *gin.Engine, addr, serverName string, stop func()) {
+	// 创建一个http.Server实例，使用Gin作为处理器
+	// -> 使用标准库http.Server配合Gin，可以更灵活地管理服务（不如自定义超时等）
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	// 在单独的Goroutine中启动服务，使主线程能够继续执行（监听信号、管理关闭逻辑等）
+	go func() {
+		log.Printf("%s running on port %s\n", serverName, addr)
+		// 捕获 ListenAndServe 的错误，除了 http.ErrServerClosed（这是正常关闭的标志），其他错误会被记录并导致程序终止。
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 捕获关闭信号，用于优雅关闭服务
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Printf("Shutdown %s ...\n", serverName)
+
+	// 关闭服务
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if stop != nil {
+		stop()
+	}
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("%s Shutdown Failed, cause by : %s\n", serverName, err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Printf("%s Shutdown timeout\n", serverName)
+	}
+	log.Printf("%s Shutdown success\n", serverName)
+}
